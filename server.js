68177547bbc231afb8f0e1c31c426456e3502c89
unknown <valentin7gubin@gmail.com>
2023-11-17 15:49:15 +0300

    add new events

--- server.js
+++ server.js
@@ -6,33 +6,47 @@ var io = require('socket.io')(server, {
   },
 });
 
-const port = process.env.PORT || 9999;
 
-// Отслеживание url адреса и отображение нужной HTML страницы
-// app.get('/', function (request, respons) {
-//   respons.sendFile(__dirname + '/index.html');
-// });
+const PORT = process.env.PORT || 9999;
+
+let users = [];
+
+app.get('/api/users', (req, res) => {
+  console.log('send', users);
+  res.send({ users });
+});
 
 io.on('connection', (socket) => {
-  console.log('Успешное соединение', socket.id);
+  const { id } = socket.client;
+  console.log('Успешное соединение', id);
+
+  socket.on('new login', (user) => {
+    users = [...users, user];
+    io.sockets.emit('new user added', user);
+    console.log(user);
+  });
+
   socket.on('disconnect', () => {
-    console.log('Отключился', socket.id);
+    console.log('Отключился', id);
   });
   socket.on('sendTime', (currentTime) => {
     socket.broadcast.emit('sync', currentTime);
   });
   socket.on('playVideo', () => {
     socket.broadcast.emit('syncPlay');
+    console.log('play');
   });
   socket.on('pauseVideo', () => {
     socket.broadcast.emit('syncPause');
+    console.log('pause');
   });
   socket.on('requestVideo', (src) => {
     io.sockets.emit('syncRequestVideo', src);
   });
 });
 
+app.set('Port', PORT);
 
-server.listen(port, () => {
-  console.log(`connect to ${port} port`);
+server.listen(PORT, () => {
+  console.log(`connect to ${PORT} port`);
 });
