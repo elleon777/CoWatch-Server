f517bfb7a0b50a426f1bfc1ee0586a08fb8c2567
elleon777 <60982813+elleon777@users.noreply.github.com>
2023-12-31 21:41:05 +0300

    Update config.ts

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,6 +1,6 @@
 import * as url from 'url';
 
 export const PORT = process.env.PORT || 9999;
-export const CLIENT_HOST = 'http://localhost:5173';
+export const CLIENT_HOST = process.env.CLIENT_HOST || 'http://localhost:5173';
 
 export const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
